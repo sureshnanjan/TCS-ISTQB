fa764a78cbeecae7ddb3fc90585876b3c5161e00
sureshnanjan <sureshnanjan@outlook.com>
2022-12-23 10:08:15 +0530

    Add project files.

---
+++ PetStoreAutomation/PetStoreHomePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PetStoreAutomation
+{
+    public class PetStoreHomePage
+    {
+        public PetStoreHomePage()
+        {
+            ChromeDriver mybrowser = new ChromeDriver();
+            mybrowser.Url = "https://petstore.octoperf.com/actions/Catalog.action";
+            Console.WriteLine(mybrowser.Title);
+        }
+    }
+}
---
+++ TCS-ISTQB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetStoreAutomation;
+namespace TCS_ISTQB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am automating Petstore");
+            PetStoreHomePage myPetstore = new PetStoreHomePage();   
+        }
+    }
+}
